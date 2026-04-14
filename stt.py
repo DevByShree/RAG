b41d5c8fcb6f1e35ae0387c0e527b55e9f6107ac
DevByShree <shreejoshi1805@gmail.com>
2026-04-14 14:12:07 +0530

    first commit

--- stt.py
+++ stt.py
@@ -1,3 +1,26 @@
 import whisper
+import json
+import os
 
 model = whisper.load_model("large-v2")
+
+result = model.transcribe(
+    r"D:\RAG\audios\1_heading-paragraphs-and-links-sigma-web-development-course-tutorial.mp3",
+    language="hi",
+    task="translate",
+    word_timestamps=False
+)
+chunks= []
+for segment in result["segments"]:
+  chunks.append({"start": segment["start"], "end": segment["end"], "text": segment["text"],"number": segment["number"],"tittle":segment["tittle"]})
+
+print(chunks)
+
+chunks_with_metadata={
+    "chunks":chunks,
+    "text":result["text"]
+}
+os.makedir("json",exist_ok=True)
+with open("output.json","w") as f:
+  json.dump(chunks_with_metadata, f,indent=4)
+
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    # tutorial_number = file.split(" [")[0].split(" #")[1]
-    # print(turorial_number)
